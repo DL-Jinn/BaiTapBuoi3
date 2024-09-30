489a158df5209e2d4692153e7176ad631ac6d757
ACER <ACER@DESKTOP-IIP4T3G>
2024-09-30 23:57:19 +0700

    Add project files.

---
+++ BaiTapBuoi3/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace BaiTapBuoi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.dtgvThongTin = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvThongTin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(418, 10);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 41);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(418, 55);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 41);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(418, 149);
+            this.btnDong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(75, 41);
+            this.btnDong.TabIndex = 3;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(418, 100);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 41);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // dtgvThongTin
+            // 
+            this.dtgvThongTin.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dtgvThongTin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvThongTin.Location = new System.Drawing.Point(9, 10);
+            this.dtgvThongTin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtgvThongTin.Name = "dtgvThongTin";
+            this.dtgvThongTin.RowHeadersWidth = 51;
+            this.dtgvThongTin.RowTemplate.Height = 24;
+            this.dtgvThongTin.Size = new System.Drawing.Size(395, 232);
+            this.dtgvThongTin.TabIndex = 4;
+            this.dtgvThongTin.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvThongTin_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 270);
+            this.Controls.Add(this.dtgvThongTin);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvThongTin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.DataGridView dtgvThongTin;
+    }
+}
+
---
+++ BaiTapBuoi3/Form2.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace BaiTapBuoi3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbMSNV = new System.Windows.Forms.TextBox();
+            this.txbLuong = new System.Windows.Forms.TextBox();
+            this.TXBTen = new System.Windows.Forms.TextBox();
+            this.btnDongY = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MSNV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(15, 94);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lương";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên";
+            // 
+            // txbMSNV
+            // 
+            this.txbMSNV.Location = new System.Drawing.Point(70, 27);
+            this.txbMSNV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txbMSNV.Name = "txbMSNV";
+            this.txbMSNV.Size = new System.Drawing.Size(268, 20);
+            this.txbMSNV.TabIndex = 3;
+            this.txbMSNV.TextChanged += new System.EventHandler(this.txbMSNV_TextChanged);
+            // 
+            // txbLuong
+            // 
+            this.txbLuong.Location = new System.Drawing.Point(70, 93);
+            this.txbLuong.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txbLuong.Name = "txbLuong";
+            this.txbLuong.Size = new System.Drawing.Size(268, 20);
+            this.txbLuong.TabIndex = 4;
+            this.txbLuong.TextChanged += new System.EventHandler(this.txbLuong_TextChanged);
+            // 
+            // TXBTen
+            // 
+            this.TXBTen.Location = new System.Drawing.Point(70, 62);
+            this.TXBTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TXBTen.Name = "TXBTen";
+            this.TXBTen.Size = new System.Drawing.Size(268, 20);
+            this.TXBTen.TabIndex = 5;
+            this.TXBTen.TextChanged += new System.EventHandler(this.TXBTen_TextChanged);
+            // 
+            // btnDongY
+            // 
+            this.btnDongY.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDongY.Location = new System.Drawing.Point(66, 150);
+            this.btnDongY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDongY.Name = "btnDongY";
+            this.btnDongY.Size = new System.Drawing.Size(94, 30);
+            this.btnDongY.TabIndex = 6;
+            this.btnDongY.Text = "Đồng ý";
+            this.btnDongY.UseVisualStyleBackColor = true;
+            this.btnDongY.Click += new System.EventHandler(this.btnDongY_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBoQua.Location = new System.Drawing.Point(234, 150);
+            this.btnBoQua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(104, 30);
+            this.btnBoQua.TabIndex = 7;
+            this.btnBoQua.Text = "Bỏ qua";
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(390, 198);
+            this.Controls.Add(this.btnBoQua);
+            this.Controls.Add(this.btnDongY);
+            this.Controls.Add(this.TXBTen);
+            this.Controls.Add(this.txbLuong);
+            this.Controls.Add(this.txbMSNV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbMSNV;
+        private System.Windows.Forms.TextBox txbLuong;
+        private System.Windows.Forms.TextBox TXBTen;
+        private System.Windows.Forms.Button btnDongY;
+        private System.Windows.Forms.Button btnBoQua;
+    }
+}
